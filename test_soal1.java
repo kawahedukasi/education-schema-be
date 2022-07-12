2bed53eff69e7711e063f09c9c09fdcf115de251
Inriveronika <109125189+Inriveronika@users.noreply.github.com>
2022-07-12 21:06:03 +0700

    inri-veronika : Test Kawah Edukasi bath III

---
+++ test_soal1.java
@@ -0,0 +1,22 @@
+Nama : inri veronika togatorop
+Alamat Email: inritogatorop369@gmail.com
+
+import java.util.stream.IntStream;
+
+public class FizzBuzz 
+{
+  public static void main(String[] args) 
+  {
+    int data=[1, 2, 4, 3, 10, 11, 20, 5, 100, 200];
+  
+    for (int data = 1; data<= 200; data++) 
+    {
+      if (((data % 5) == 0) 
+        System.out.println("Fizz");
+      else if ((data % 11) == 0) 
+        System.out.println("Buzz");
+      else ((data % 5) == 0)&& (data % 11)) 
+        System.out.println("FizzBuzz");
+      
+    }
+  }
