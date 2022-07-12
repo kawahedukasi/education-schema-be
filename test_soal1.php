b6c0ac6f88d84a0628936d70459d027d91f6326e
fiqih Kholifah <79885422+FiqihK@users.noreply.github.com>
2022-07-12 09:17:14 +0700

    fiqih_kholifah : Test Kawah Edukasi bath III

---
+++ test_soal1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    $data = array(1, 2, 4, 3, 10, 11, 20, 5, 100, 200);
+
+    foreach ($data as $nilai)
+        if ($nilai == "5") {
+            echo "Fizz  <br>";
+        } elseif ($value ==  "11") {
+            echo "Buzz  <br>";
+        } else {
+            echo "$value <br>";
+        }
+
+
+    ?>
+
+</body>
+
+</html>
