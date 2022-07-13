be79facaf650a47b6903647a5c28d3e768f5983f
MuhammadDiwan <109192772+MuhammadDiwan@users.noreply.github.com>
2022-07-13 12:44:52 +0700

    Update Test_soal1.java

--- Test_soal1.java
+++ Test_soal1.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 
 public class Test_soal1 {
     public static void main(String[] args) {
-   for(int i = 1; i <= 200; i++) {           // count from 1 to 100
-            if (((i % 5) == 0) && ((i % 11) == 0)) // Is it a multiple of 5 & 7?
+   for(int i = 1; i <= 200; i++) {           
+            if (((i % 5) == 0) && ((i % 11) == 0)) 
                 System.out.print("fizzbuzz");   
-            else if ((i % 5) == 0)                // Is it a multiple of 5?
+            else if ((i % 5) == 0)               
                 System.out.print("fizz"); 
-            else if ((i % 11) == 0)                // Is it a multiple of 7?
+            else if ((i % 11) == 0) 
                 System.out.print("buzz"); 
-            else System.out.print(i);             // Not a multiple of 5 or 7
+            else System.out.print(i);
             System.out.print(" ");                
         }
         System.out.println( );
