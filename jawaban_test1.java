06a1b39ead1b633c839c5b6769126b54baac455f
Gufron Nurjalil <94749100+Gufron14@users.noreply.github.com>
2022-07-18 17:49:13 +0700

    Update jawaban_test1.java

--- jawaban_test1.java
+++ jawaban_test1.java
@@ -2,7 +2,7 @@ public class soal_test1 {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 200; i++){
-            if (i % 5 == 0 && i % 11 == 0){
+            if ((i % 5 == 0) && (i % 11 == 0)){
                 System.out.println("FizzBuzz");
             } else if (i % 5 == 0) {
                 System.out.println("Fizz");
