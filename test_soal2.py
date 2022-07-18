c83f1c9db6bfb750516bcf584c9bddbd1bcfef4b
Ahmad Fakih Abdulwahab <109074873+Aakih@users.noreply.github.com>
2022-07-18 20:02:51 +0700

    Add files via upload

---
+++ test_soal2.py
@@ -0,0 +1,18 @@
+## Nama : Ahmad Fakih Abdulwahab
+## Alamat Email: fakihabdulwahab@gmail.com  
+## Waktu Mulai : 19.20
+## Waktu Selesai : 19.55
+
+
+data = [1, "ka", 67, "wah", "1772", "edukasi", 7, 98, -1]
+a, b, c, d, e, f, g, h, i = data
+
+print("index ke 0 adalah ",type(a), "dengan data ", a)
+print("index ke 1 adalah ",type(b), "dengan data ", b)
+print("index ke 2 adalah ",type(c), "dengan data ", c)
+print("index ke 3 adalah ",type(d), "dengan data ", d)
+print("index ke 4 adalah ",type(e), "dengan data ", e)
+print("index ke 5 adalah ",type(f), "dengan data ", f)
+print("index ke 6 adalah ",type(g), "dengan data ", g)
+print("index ke 7 adalah ",type(h), "dengan data ", h)
+print("index ke 8", "Tidak bisa di validasi sistem")
