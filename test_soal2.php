88468af2bb37430866a9a640ea5055992c8e4f05
Yahya <yahyatruth@gmail.com>
2022-07-18 20:30:16 +0700

    update answer to php

---
+++ test_soal2.php
@@ -0,0 +1,13 @@
+<?php
+$data = [1, "ka", 67, "wah", "1772", "edukasi", 7, 98, -1];
+for ($i = 0; $i < count($data); $i++) {
+    if ($data[$i] > 0) {
+        $j = $i + 1;
+        echo "index ke " . $j . " adalah " . gettype($data[$i]) . " dengan data " . " $data[$i]";
+        echo "<br />";
+    } else {
+        $j = $i + 1;
+        echo "index ke " . $j . " Tidak bisa divalidasi sistem <br>";
+    }
+}
+?>
